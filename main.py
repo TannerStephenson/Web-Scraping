b6a1eb6baac0aa2af5714da9bed4d3a5cbb0b3f9
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-01-21 13:23:45 -0800

    Added menu + more

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import re
+import tkinter as tk
 from bs4 import BeautifulSoup
 
 website = 'http://quotes.toscrape.com/'
@@ -7,6 +9,28 @@ result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
+
+
+def tag_scrape():
+    tag = soup.find_all('span', class_='tag-item')
+    print("Please choose a tag to sort by:")
+
+    for tag in tag:
+        individual_tag = tag.find('a')
+        tag_url = individual_tag['href']
+        match = re.search(r'tag/(.+)/', tag_url)
+        if match:
+            print(match.group(1))
+
+
+def print_console():
+    boxes = soup.find_all('div', class_='quote')
+
+    for box in boxes:
+        quote = box.find('span', class_='text').get_text()
+        author = box.find('small', class_='author').get_text()
+        print(quote)
+        print(author)
 
 
 def csv_scrape():
@@ -26,6 +50,44 @@ def csv_scrape():
     df.to_csv('quotes.csv', index=False)
 
 
-csv_scrape()
+def about():
+    print()
+    print("Hi, my name is Tanner Stephenson.")
+    print()
+    print("I am just testing out python's web scraping capabilities.")
+    print("I'm using quotes.toscrape.com for this test program")
+    print()
 
 
+def menu():
+    print("Welcome to my Web-Scraper")
+    print("-------------------------")
+
+    choice = input("""'1': "Print to console
+'2': "Create and save a csv file
+'3': 'Sort by tag'
+'4': 'About'
+'5': "Exit
+Please select: """)
+    print(choice)
+
+    if choice == '1':
+        print_console()
+        print()
+        menu()
+    elif choice == '2':
+        csv_scrape()
+        menu()
+    elif choice == '3':
+        tag_scrape()
+        menu()
+    elif choice == '4':
+        about()
+        menu()
+    elif choice == '5':
+        exit()
+    else:
+        print("Unknown Option Selected!")
+
+
+menu()
